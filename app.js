accc387ed5e4df1117e19e42ebe72465622492f5
sundeepamit <sundeepamitmaurya@gmail.com>
2026-01-02 12:01:36 +0530

    able to create new recipe page and store in databases

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 // routes
 app.use('/recipe', router)
--- routes/recipe.js
+++ routes/recipe.js
@@ -18,4 +18,34 @@ router.get('/detail/:id', async (req, res) => {
     const recipe = await Recipe.findById(id)
     res.render('recipes/detail', { recipe })
 })
+
+router.get('/new', (req, res) => {
+    res.render('recipes/new')
+})
+
+router.post('/new', async (req, res) => {
+    let { title, author, totalTime, image, description, ingredients, steps } = req.body
+    if (ingredients) {
+        ingredients = ingredients
+            .split('\n')                    // split by lines
+            .map(line => line.trim())       // remove useless spaces
+            .filter(line => line.length > 0); // remove empty lines
+    }
+
+    if (steps) {
+        steps = steps
+            .split('\n')
+            .map(line => line.trim())
+            .filter(line => line.length > 0);
+    }
+    if (image) {
+        image = image
+            .split('\n')
+            .map(line => line.trim())
+            .filter(line => line.length > 0);
+    }
+    const recipe = await Recipe.insertOne({ title, author, totalTime, image, description, ingredients, steps })
+
+    res.redirect(`/recipe/detail/${recipe._id}`)
+})
 module.exports = router
